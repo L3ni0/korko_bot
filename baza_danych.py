349747ec2a72313472e69a340b9de43d9ec209e8
L3ni0 <dominiklenio@gmail.com>
2021-07-13 11:37:19 +0400

    wstawianie

---
+++ baza_danych.py
@@ -0,0 +1,212 @@
+# %%
+import sqlite3
+from datetime import date
+
+conn = sqlite3.connect('baza.db')
+conn.execute("PRAGMA foreign_keys = 1")  # relations won't work without that
+c = conn.cursor()
+
+
+def creating_tables():
+    """creating 3 tables with we will use in this database
+    table students have 5 values:
+    student_id: id from discord
+    st_name, st_last_name,st_phone: personal information
+    st_status: status if we still have lessons with this student or not
+
+    table tutor have 3 values:
+    tutor_id: tutor discord id
+    tutor_name,tutor_last_name: presonal information
+
+    table lessons have 5 values:
+    lesson_id: id of lesson
+    tutor_id: id of tutors who's have lesson
+    student_id: id of student who's have lesson
+    start_time: time and day when lesson started
+    course_duration: how long lesson was"""
+    c.execute('''
+                CREATE TABLE students(
+                student_id INTEGER PRIMARY KEY, 
+                st_name TEXT, 
+                st_last_name TEXT, 
+                st_phone TEXT,
+                st_discord TEXT, 
+                st_status TEXT)
+                ''')
+
+    c.execute('''
+                CREATE TABLE tutors(
+                tutor_id  INTEGER PRIMARY KEY, 
+                tutor_name TEXT, 
+                tutor_last_name TEXT,
+                tutor_discord TEXT)
+                ''')
+
+    c.execute("""
+                CREATE TABLE lessons(
+                lesson_id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                tutor_id INTEGER , 
+                student_id INTEGER ,
+                start_day TIME, 
+                course_duration REAL,
+                FOREIGN KEY (tutor_id) REFERENCES tutors(tutor_id),
+                FOREIGN KEY (student_id) REFERENCES students(student_id))
+                """)
+    conn.commit()
+
+
+def add_student(st_name, st_last_name, st_phone, st_discord, st_status=""):
+    tab = [st_discord.split('#')[1], st_name, st_last_name, st_phone, st_discord, st_status]
+
+    c.execute(f"""
+                    INSERT INTO students(student_id,st_name, st_last_name, st_phone, st_discord, st_status) 
+                    VALUES (?,?,?,?,?,?)""", tab)
+    conn.commit()
+
+
+def add_tutor(name, last_name, tutor_discord):
+    tab = [tutor_discord.split('#')[1], name, last_name, tutor_discord]
+    c.execute(f'''INSERT INTO tutors(tutor_id,tutor_name, tutor_last_name, tutor_discord) VALUES 
+                (?,?,?,?)''', tab)
+    conn.commit()
+
+
+def add_lesson(tutor_id, student_id, curse_duration=1.0, start_day=date.today()):
+    tab = [tutor_id, student_id, curse_duration, start_day]
+    c.execute(f"""
+                INSERT INTO  lessons(tutor_id, student_id, course_duration, start_day)
+                VALUES  (?,?,?,?)
+                """, tab)
+    conn.commit()
+
+
+def show_students():
+    c.execute(f"""
+        SELECT * FROM students
+        """)
+    info = c.fetchall()
+    conn.commit()
+    return info
+
+
+def show_my_students(tutor_id):
+    c.execute(f"""
+            SELECT st_name, st_last_name, st_phone, st_discord, st_status
+            FROM lessons
+            INNER JOIN students 
+                ON students.student_id = lessons.student_id
+            WHERE tutor_id = ?
+            GROUP BY st_discord
+            """, [tutor_id])
+
+    info = c.fetchall()
+    conn.commit()
+    return info
+
+
+def show_tutors():
+    c.execute(f"""
+        SELECT * FROM tutors
+        """)
+    info = c.fetchall()
+    conn.commit()
+    return info
+
+
+def show_lessons(month):
+    c.execute(f"""
+        SELECT lesson_id, start_day, course_duration, st_name, st_last_name 
+        FROM lessons
+        INNER JOIN students 
+                ON students.student_id = lessons.student_id
+        WHERE start_day LIKE ?
+        """, ["%-" + month + "-%"])
+    info = c.fetchall()
+    conn.commit()
+    return info
+
+
+def show_my_lesson(tutor_id, month=date.today().strftime("%m")):
+    c.execute(f"""
+            SELECT lesson_id, start_day, course_duration, st_name, st_last_name
+            FROM lessons
+            INNER JOIN students 
+                ON students.student_id = lessons.student_id
+            WHERE tutor_id = ? AND start_day LIKE ?
+            ORDER BY start_day
+            """, [tutor_id, "%-" + month + "-%"])
+
+    info = c.fetchall()
+    conn.commit()
+    return info
+
+
+def count_hours(tutor_id, month=date.today().strftime("%m")):
+    c.execute(f"""
+                SELECT st_discord,st_name, st_last_name, SUM(course_duration)
+                FROM lessons
+                INNER JOIN students 
+                    ON students.student_id = lessons.student_id
+                WHERE tutor_id = ? AND start_day LIKE ?
+                GROUP BY st_discord
+                """, [tutor_id, "%-" + month + "-%"])
+    info = c.fetchall()
+    conn.commit()
+    return info
+
+
+def change_lesson(tutor_id, student_id, lesson_id, date, time):
+    c.execute('''
+                UPDATE lessons
+                SET student_id = ?,
+                    start_day = ?,
+                    course_duration = ?
+                WHERE
+                    tutor_id = ? AND lesson_id = ? AND student_id = ?
+                
+    ''', ([student_id, date, time, tutor_id, lesson_id, student_id]))
+    conn.commit()
+
+
+def change_student(st_discord, name, last_name, phone_nub, status):
+    c.execute('''
+            UPDATE students
+            SET st_name = ?,
+                st_last_name = ?,
+                st_phone = ?,
+                st_status = ?,
+            WHERE 
+                st_id = ?
+            ''', ([name, last_name, phone_nub, status, st_discord.split('#')[1]]))
+    conn.commit()
+
+
+def change_tutor(tutor_discord, name, last_name):
+    c.execute('''
+            UPDATE tutors
+            SET st_name = ?,
+                st_last_name = ?
+            WHERE
+                tutor_id = ?
+            ''', ([tutor_discord.split('#')[1], name, last_name]))
+    conn.commit()
+
+
+def delete_student(st_discord):
+    c.execute('''
+            DELETE FROM students
+            WHERE student_id = ? 
+            ''', ([st_discord.split('#')[1]]))
+    conn.commit()
+
+
+def delete_tutor(tutor_discord):
+    c.execute('''
+            DELETE FROM tutors
+            WHERE tutor_id = ?   
+            ''', (tutor_discord.split('#')[1]))
+    conn.commit()
+
+
+if __name__ == '__main__':
+    add_lesson("937", "6214", 2.4)
---
+++ bot.py
@@ -0,0 +1,151 @@
+import discord
+import datetime
+import os
+from baza_danych import *
+from discord.ext import commands
+
+
+client = commands.Bot(command_prefix="..")
+client.remove_command('help')
+
+
+@client.event
+async def on_ready():
+    print(f'everything is ok {client.user}')
+
+
+@client.command()
+async def help(ctx):
+    embed = discord.Embed(title="Komendy",
+                          description="komendy tylko dla nauczycieli, wy nie musicie się nimi przejmować :p..."
+                                      "...............          \elementy opcjonalne\ ", color=0x0088ff)
+    embed.add_field(name="..leckja @osoba \czas\ \dzień\ ",
+                    value="czas = 1(podajemy w godzinach) \ndzien = dzisiaj w formacie RRRR - MM - DD", inline=True)
+    embed.add_field(name="..dodaj_studenta @osoba imie nazwisko numer_tel status",
+                    value="status przyda się później jak będziecie zawieszać zajęcia czy coś", inline=True)
+    embed.add_field(name="..moje_lekcje \miesiąc\ ", value="miesiąc = obecny miesiąc format MM", inline=True)
+    embed.add_field(name="..moi_uczniowie", value="zwraca tabelę z osobami z którymi mieliśmy zajęcia", inline=True)
+    embed.add_field(name="..zmien_lekcje @osoba id_lekcji data czas",
+                    value="czas i data to są wartości które są zmieniane", inline=True)
+    await ctx.author.send(embed=embed, delete_after=120)
+
+
+@client.command()
+@commands.has_role("tutor")
+async def lekcja(ctx, member : discord.Member, dur=1.0, day=str(datetime.date.today())):
+    add_lesson(str(ctx.author).split('#')[1],str(member).split('#')[1], dur, str(day))
+    await ctx.author.send(f'{ctx.author} rozpoczołeś lekcje z {member} na czas {dur}')
+
+
+@client.command()
+@commands.has_role("tutor")
+async def dodaj_ucznia(ctx, member : discord.Member, imie, nazwisko, numer_telefonu, status=''):
+    add_student(imie, nazwisko, numer_telefonu, str(member), status)
+    await ctx.author.send(f'{member}, {imie}, {nazwisko}, {numer_telefonu} zostal dodany :D', delete_after=60)
+
+
+@client.command()
+@commands.has_role("tutor")
+async def moje_lekcje(ctx, month=datetime.date.today().strftime("%m")):
+    info = show_my_lesson(str(ctx.author).split('#')[1], month)
+    embed = discord.Embed()
+    for row in info:
+        embed.add_field(name=f"{row[1]}", value=f'{row[0]}, {row[3]} {row[4]}, {row[2]}', inline=False)
+    await ctx.author.send(embed=embed)
+
+
+@client.command()
+@commands.has_role("tutor")
+async def moi_uczniowie(ctx):
+    students = show_my_students(str(ctx.author).split('#')[1])
+    embed = discord.Embed()
+    for row in students:
+        embed.add_field(name=f"{row[0]} {row[1]}", value=f'{row[2]} {row[3]}, {row[4]}', inline=False)
+    await ctx.author.send(embed=embed)
+
+
+@client.command()
+@commands.has_role("tutor")
+async def zmien_lekcje(ctx, member : discord.Member, lesson_id, date, time):
+    change_lesson(str(ctx.author).split('#')[1],str(member).split('#')[1], lesson_id, date, time)
+    await ctx.author.send(f'lekcja została zmieniona')
+
+
+@client.command()
+@commands.has_role("tutor")
+async def zmien_dane_ucznia(ctx, member : discord.Member, imie, nazwisko, numer_tel, status):
+    change_student(member, imie, nazwisko, numer_tel, status)
+    await ctx.author.send(f"{member} dane zostały zmienione")
+
+
+@client.command()
+@commands.has_role("tutor")
+async def usun_ucznia(ctx, member: discord):
+    delete_student(member)
+    await ctx.author.send(f'{member} został usunięty z listy uczniów')
+
+
+
+@client.command()
+@commands.has_role("adminn")
+async def dodaj_nauczyciela(ctx, member : discord.Member, imie, nazwisko):
+    add_tutor(imie, nazwisko, str(member))
+    await ctx.author.send(f'{member}, {imie}, {nazwisko} tutor zostal dodany :D', delete_after=60)
+
+
+@client.command()
+@commands.has_role("adminn")
+async def lekcje(ctx, month=datetime.date.today().strftime("%m")):
+    lessons = show_lessons(month)
+    embed = discord.Embed()
+    for row in lessons:
+        embed.add_field(name=f"{row[1]}", value=f'{row[0]}, {row[3]} {row[4]}, {row[2]}', inline=False)
+    await ctx.author.send(embed=embed)
+
+
+@client.command()
+@commands.has_role("adminn")
+async def nauczyciele(ctx):
+    nauczyciele = show_tutors()
+    embed = discord.Embed()
+    for row in nauczyciele:
+        embed.add_field(name=f"{row[0]}", value=f'{row[1]}, {row[2]}', inline=False)
+    await ctx.author.send(embed=embed)
+
+
+@client.command()
+@commands.has_role("adminn")
+async def uczniowie(ctx):
+    students = show_students()
+    embed = discord.Embed()
+    for row in students:
+        embed.add_field(name=f"{row[0]} {row[1]}", value=f'{row[2]} {row[3]}, {row[4]}', inline=False)
+    await ctx.author.send(embed=embed)
+
+
+@client.command()
+@commands.has_role("adminn")
+async def policz_godziny(ctx, member : discord.Member, month=date.today().strftime("%m")):
+        hours = count_hours(str(member).split('#')[1], month)
+        godziny = sum([float(info[3]) for info in hours])
+
+        embed = discord.Embed()
+        embed.add_field(name=f"liczba przepracowanych godzin {member} w {month} miesiącu = {godziny}",
+                        value="\n".join([str(info).replace('(', '').replace(')', '') for info in hours]))
+
+        await ctx.author.send(embed=embed)
+
+@client.command()
+@commands.has_role("adminn")
+async def zmien_dane_nauczyciela(ctx, member : discord.Member, imie, nazwisko):
+    change_tutor(member, imie, nazwisko)
+    await ctx.author.send(f"{member} dane zostały zmienione")
+
+@client.command()
+@commands.has_role("adminn")
+async def usun_nauczyciela(ctx, member : discord.Member):
+    delete_tutor(member)
+    await  ctx.author.send(f"{member} dane nauczyciela zostały zmienione")
+
+
+client.run(os.getenv('TOKEN'))
